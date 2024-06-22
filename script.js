ecec26b119b0ec552c1defec87f5c4c87de11070
srujana-porandla <131435996+srujana-porandla@users.noreply.github.com>
2024-06-22 19:00:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function functionName() {
+	alert(arguments.callee.name);
+}
+functionName();
+function AccioJob(){
+	alert(arguments.callee.name);
+}
+AccioJob();
